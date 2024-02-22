f87d187b9a293f87ce114fe6bab8b47dd86ee3ce
sskesu <136444697+sskesu@users.noreply.github.com>
2024-02-22 11:02:36 +0900

    Feat : 플레이어 공격 적용

--- Assets/ScriptableObjects/Scripts/PlayerAttackData.cs
+++ Assets/ScriptableObjects/Scripts/PlayerAttackData.cs
@@ -12,6 +12,8 @@ public class AttackInfoData
     [field: SerializeField][field: Range(0f, 1f)] public float ComboTransitionTime { get; private set; }
     [field: SerializeField][field: Range(0f, 3f)] public float ForceTransitionTime { get; private set; }
     [field: SerializeField][field: Range(-10f, 10f)] public float Force { get; private set; }
+    [field: SerializeField][field: Range(0f, 1f)] public float Dealing_Start_TransitionTime { get; private set; }
+    [field: SerializeField][field: Range(0f, 1f)] public float Dealing_End_TransitionTime { get; private set; }
 
     [field: SerializeField] public int Damage { get; private set; }
 }
--- Assets/Scripts/Characters/Player/StateMachines/PlayerAttackState.cs
+++ Assets/Scripts/Characters/Player/StateMachines/PlayerAttackState.cs
@@ -1,15 +1,65 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerAttackState : PlayerBaseState
 {
+    private bool alreadyAppliedForce;
+    private bool alreadyAppliedDealing;
+
     public PlayerAttackState(PlayerStateMachine playerStateMachine) : base(playerStateMachine)
     {
     }
 
+
+    public override void Update()
+    {
+        base.Update();
+
+        ForceMove();
+
+        float normalizedTime = GetNormalizedTime(stateMachine.Player.Animator, "Attack");
+        if (normalizedTime < 1f)
+        {
+            if (normalizedTime >= stateMachine.Player.Data.AttackData.GetAttackInfo(stateMachine.ComboIndex).ForceTransitionTime) // ¿Œµ¶Ω∫
+                TryApplyForce();
+
+            if (!alreadyAppliedDealing && normalizedTime >= stateMachine.Player.Data.AttackData.GetAttackInfo(stateMachine.ComboIndex).Dealing_Start_TransitionTime) // ¿Œµ¶Ω∫
+            {
+                stateMachine.Player.Weapon.SetAttack(stateMachine.Player.Data.AttackData.GetAttackInfo(stateMachine.ComboIndex).Damage,
+                    stateMachine.Player.Data.AttackData.GetAttackInfo(stateMachine.ComboIndex).Force); // ¿Œµ¶Ω∫ 2∞≥
+                Debug.Log("«√∑π¿ÃæÓæÓ≈√");
+                stateMachine.Player.Weapon.gameObject.SetActive(true);
+                alreadyAppliedDealing = true;
+            }
+
+            if (alreadyAppliedDealing && normalizedTime >= stateMachine.Player.Data.AttackData.GetAttackInfo(stateMachine.ComboIndex).Dealing_End_TransitionTime) // ¿Œµ¶Ω∫
+            {
+                stateMachine.Player.Weapon.gameObject.SetActive(false);
+            }
+
+        }
+    }
+
+    private void TryApplyForce()
+    {
+        if (alreadyAppliedForce) return;
+        alreadyAppliedForce = true;
+
+        stateMachine.Player.ForceReceiver.Reset();  
+
+        stateMachine.Player.ForceReceiver.AddForce(stateMachine.Player.transform.forward * 
+            stateMachine.Player.Data.AttackData.GetAttackInfo(stateMachine.ComboIndex).Force); // ¿Œµ¶Ω∫
+
+    }
+
     public override void Enter()
     {
+        alreadyAppliedForce = false;
+        alreadyAppliedDealing = false;
+
         stateMachine.MovementSpeedModifier = 0;
         base.Enter();
 
